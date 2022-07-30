2be7ed8b1dc2a47f9ee1591e0262becdb29d7999
z33shangk <94767300+z33shangk@users.noreply.github.com>
2022-07-30 12:11:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,20 +22,22 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#streamlit.write('The user entered ', fruit_choice)
+#New section to display fruitvice api response
+#streamlit.text(fruityvice_response.json()) #write the json data to screen as is
+#take json response and normalize it
+#out the screen as table
+
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information.")
-else:
-#streamlit.write('The user entered ', fruit_choice)
-#New section to display fruitvice api response
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #write the json data to screen as is
-#take json response and normalize it
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#out the screen as table
-  streamlit.dataframe(fruityvice_normalized)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
