794a8a22487b812f8cd07a76268d65eb209fd034
z33shangk <94767300+z33shangk@users.noreply.github.com>
2022-07-30 11:56:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 #import pandas
 #import requests
 import snowflake.connector
-from urlib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast menu')
